64c38c1a8ad07b421639e1f4f9101f4def40f010
nm777taka <nm7.taka@gmail.com>
2013-09-04 15:53:43 +0900

    InsertViewを追加

---
+++ InsertViewController.m
@@ -0,0 +1,38 @@
+//
+//  InsertViewController.m
+//  gitTodo
+//
+//  Created by 古田 貴久 on 2013/09/04.
+//  Copyright (c) 2013年 古田 貴久. All rights reserved.
+//
+
+#import "InsertViewController.h"
+
+@interface InsertViewController ()
+
+@end
+
+@implementation InsertViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
