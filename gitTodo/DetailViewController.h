712220a45fc079c74839ab255d7181164672b316
nm777taka <nm7.taka@gmail.com>
2013-09-04 15:37:21 +0900

    DetailViewを追加

---
+++ gitTodo/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  gitTodo
+//
+//  Created by 古田 貴久 on 2013/09/04.
+//  Copyright (c) 2013年 古田 貴久. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
